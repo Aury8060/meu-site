8aaac1a3087fac8c952e60e9a2703cda05a379a9
Aury8060 <patricksilva8060@gmail.com>
2025-06-04 22:19:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const texto = "Nunca desista dos seus sonhos, tenha foco no seu trabalho e dedique-se ao máximo, pois tudo é possível para aquele que crê. (Marcos 9:23)";
+    document.getElementById("mensagem").innerText = texto;
+
+}
